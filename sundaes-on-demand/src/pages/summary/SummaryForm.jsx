07bba910289304d333bafff7fa762827abaf2322
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2023-05-12 17:45:47 -0600

    L42: React Code: SummaryForm Checkbox and Button

--- sundaes-on-demand/src/pages/summary/SummaryForm.jsx
+++ sundaes-on-demand/src/pages/summary/SummaryForm.jsx
@@ -1,3 +1,30 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
 export default function SummaryForm() {
-  return <div />;
+  const [tcChecked, setTcChecked] = useState(false);
+  const { Group, Check } = Form;
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{ color: "blue" }}>Terms and Conditions</span>
+    </span>
+  );
+
+  return (
+    <Form>
+      <Group controlId="terms-and-conditions">
+        <Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm Order
+      </Button>
+    </Form>
+  );
 }
