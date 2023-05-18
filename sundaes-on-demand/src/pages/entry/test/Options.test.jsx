1564c994782ae572c84282c829a18c9879a64ae3
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2023-05-17 18:58:28 -0600

    L58: Code Quiz! Topping Options from Server.

--- sundaes-on-demand/src/pages/entry/test/Options.test.jsx
+++ sundaes-on-demand/src/pages/entry/test/Options.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 
-test("displays images for each scoop option from server", async () => {
+test("Displays images for each scoop option from server", async () => {
   render(<Options optionType="scoops" />);
 
   // find images
@@ -11,4 +11,22 @@ test("displays images for each scoop option from server", async () => {
   // confirm alt text of images
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("Displays images for each topping option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  // find images
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  // confirm alt text of images
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
 });
