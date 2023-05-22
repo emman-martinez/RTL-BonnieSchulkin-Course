7784df83a72ee6bb8ba2f14f7e0d8e93a3dde526
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2023-05-22 17:11:48 -0600

    L70: OPTIONAL React Code: Use Context to Display Scoops Subtotal

--- sundaes-on-demand/src/index.js
+++ sundaes-on-demand/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/pages/summary/summaryform.css";
+import "./styles/pages/entry/scoopoption.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- sundaes-on-demand/src/pages/entry/ScoopOption.jsx
+++ sundaes-on-demand/src/pages/entry/ScoopOption.jsx
@@ -1,13 +1,34 @@
 import Col from "react-bootstrap/Col";
+import { Form, Row } from "react-bootstrap";
+import { useOrderDetails } from "../../context/OrderDetails";
 
 export default function ScoopOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails();
+  const { Control, Group, Label } = Form;
+
+  const handleChange = (e) => {
+    updateItemCount(name, parseInt(e.target.value), "scoops");
+  };
+
   return (
-    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+    <Col className="scoopoption__main-col" lg={3} md={4} sm={6} xs={12}>
       <img
         alt={`${name} scoop`}
+        className="scoopoption__img"
         src={`http://localhost:3030/${imagePath}`}
-        style={{ width: "75%" }}
       />
+      <Group
+        as={Row}
+        className="scoopoption__group"
+        controlId={`${name}-count`}
+      >
+        <Label className="scoopoption__label" column xs="6">
+          {name}
+        </Label>
+        <Col className="scoopoption__number " xs="5">
+          <Control defaultValue={0} onChange={handleChange} type="number" />
+        </Col>
+      </Group>
     </Col>
   );
 }
