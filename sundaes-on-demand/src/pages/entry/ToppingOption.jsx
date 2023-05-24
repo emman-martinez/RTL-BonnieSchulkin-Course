1e7b90de7b7babcf0fde3cf784a09f8c30a3745b
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2023-05-23 18:19:25 -0600

    L76: OPTIONAL React Code: Toppings Checkboxes

--- sundaes-on-demand/src/pages/entry/ToppingOption.jsx
+++ sundaes-on-demand/src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,14 @@
-import Col from "react-bootstrap/Col";
+import { Col, Form } from "react-bootstrap";
+import { useOrderDetails } from "../../context/OrderDetails";
 
 export default function ToppingOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails();
+  const { Check, Group } = Form;
+
+  const handleChange = (e) => {
+    updateItemCount(name, e.target.checked ? 1 : 0, "toppings");
+  };
+
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
@@ -8,6 +16,9 @@ export default function ToppingOption({ name, imagePath }) {
         src={`http://localhost:3030/${imagePath}`}
         style={{ width: "75%" }}
       />
+      <Group controlId={`${name}-topping-checkbox`}>
+        <Check type="checkbox" onClick={handleChange} label={name} />
+      </Group>
     </Col>
   );
 }
